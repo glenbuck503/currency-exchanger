301e639d250c38b4981a86c935e714e8f26bf5b3
glenbuck503 <glenbuck503@gmail.com>
2021-02-13 21:47:58 -0800

    add UI logic to show users input in console

--- src/main.js
+++ src/main.js
@@ -2,4 +2,20 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Currency from './exchange-service.js';
+// import Currency from './exchange-service.js';
+
+
+
+
+
+
+
+
+
+$('#rates').submit(function() {
+  event.preventDefault();
+
+  let userInput = $('#userInput').val();
+  console.log(userInput);
+
+});
